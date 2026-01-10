aba94f3afce34a5f3bd60e5bc2f4ac560034574e
FellyKa <fellyka@gmail.com>
2026-01-11 00:45:41 +0200

    How to read the appsettings.json file

--- CompanyHRandLeaveManagementSystem/Program.cs
+++ CompanyHRandLeaveManagementSystem/Program.cs
@@ -1,6 +1,34 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//Add services for MVC
+builder.Services.AddControllersWithViews();
+
+//Access configuration
+var myValue = builder.Configuration["MyKey"];
+Console.WriteLine($"MyKey = {myValue}");
+
+
+
+
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+/*app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");*/
+
+app.MapGet("/", () => $"MyKey : {myValue}");
 
 app.Run();
