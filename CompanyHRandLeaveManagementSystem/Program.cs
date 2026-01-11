7c157287f8073faa2c27053cec2bf47d39367eee
FellyKa <fellyka@gmail.com>
2026-01-11 10:32:14 +0200

    Adding good structure and comments in the Program.cs

--- CompanyHRandLeaveManagementSystem/Program.cs
+++ CompanyHRandLeaveManagementSystem/Program.cs
@@ -1,3 +1,5 @@
+//This lines create a WebApplicationBuilder instance used to configure the web application
+// It sets up services, middleware, and other configurations needed for the application to run.
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services for MVC
@@ -8,12 +10,9 @@ var myValue = builder.Configuration["MyKey"];
 Console.WriteLine($"MyKey = {myValue}");
 
 
-
-
-
-
 var app = builder.Build();
 
+//Configure the HHTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -21,14 +20,19 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+//Serves static files from the wwwroot folder
 app.UseStaticFiles();
 
 app.UseRouting();
 app.UseAuthorization();
+
+//Map endpoints for controllers
 /*app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");*/
 
 app.MapGet("/", () => $"MyKey : {myValue}");
 
+//This line starts the web application and begins listening for incoming HTTP requests
 app.Run();
